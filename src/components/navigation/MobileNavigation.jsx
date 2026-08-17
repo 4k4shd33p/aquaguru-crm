316bf901c3707fbb8942fe58727cd94c3eaf0377
4k4shd33p <akashd33p.v@gmail.com>
2026-08-17 23:35:28 +0530

    feat: add Installation frontend workflows

--- src/components/navigation/MobileNavigation.jsx
+++ src/components/navigation/MobileNavigation.jsx
@@ -1,4 +1,4 @@
-import { Boxes, ChartNoAxesCombined, House, Menu, UsersRound } from 'lucide-react'
+import { Boxes, ChartNoAxesCombined, ClipboardCheck, House, UsersRound } from 'lucide-react'
 import { NavLink } from 'react-router-dom'
 
 export function MobileNavigation() {
@@ -7,7 +7,8 @@ export function MobileNavigation() {
     <NavLink to="/customers"><UsersRound size={19} /><span>Customers</span></NavLink>
     <NavLink to="/equipment"><Boxes size={19} /><span>Equipment</span></NavLink>
     <NavLink to="/sales"><ChartNoAxesCombined size={19} /><span>Sales</span></NavLink>
-    <button disabled title="More is coming soon"><Menu size={19} /><span>More</span></button>
+    <NavLink to="/installations"><ClipboardCheck size={19} /><span>Install</span></NavLink>
   </nav>
 }
+
 
